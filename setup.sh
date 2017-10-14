a64cb9f23b9462063b60ed188a65e07c3d8af38a
kuntaro <kuntaro1480@gmail.com>
2017-10-14 15:23:09 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 sudo cp nginx.conf /etc/nginx/
 sudo cp isucon.conf /etc/nginx/sites-available/
-sudo systemctl reload nginx
+sudo systemctl restart nginx
 
 sudo cp my.cnf /etc/mysql/my.cnf
-sudo systemctl reload mysql
+sudo systemctl restart mysql
+
+sudo systemctl restart isu-ruby
