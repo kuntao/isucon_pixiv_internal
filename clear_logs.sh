3f0e86ffd7c3eed07223b0d29eba39c28fca9469
kuntaro <kuntaro1480@gmail.com>
2017-10-14 16:10:22 +0900

    fix

--- clear_logs.sh
+++
@@ -1,6 +0,0 @@
-set -ex
-
-now=`date +%Y%m%d-%H%M%S`
-mv /var/log/nginx/access.log /var/log/nginx/access.log.$now # nginxのログをローテート
-mv /var/log/mysql/slow.log /var/log/mysql/slow.log.$now # mysqlのslowlogをローテート
-
--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 set -ex
 
+now=`date +%Y%m%d-%H%M%S`
+mv /var/log/nginx/access.log /var/log/nginx/access.log.$now # nginxのログをローテート
+mv /var/log/mysql/slow.log /var/log/mysql/slow.log.$now # mysqlのslowlogをローテート
+
 cp nginx.conf /etc/nginx/
 cp isucon.conf /etc/nginx/sites-available/
 systemctl restart nginx
